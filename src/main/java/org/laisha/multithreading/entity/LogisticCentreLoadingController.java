cb0bce1452d9eb31a3fa42a13e7d6f68eaa5adb4
kiryl-laisha <laishakaya@gmail.com>
2022-04-04 11:10:13 +0300

    AtomicInteger was changed by int for variable currentLoading.

--- src/main/java/org/laisha/multithreading/entity/LogisticCentreLoadingController.java
+++ src/main/java/org/laisha/multithreading/entity/LogisticCentreLoadingController.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.TimerTask;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class LogisticCentreLoadingController extends TimerTask {
 
@@ -21,20 +20,20 @@ public class LogisticCentreLoadingController extends TimerTask {
 
         logger.log(Level.DEBUG, "Loading controller has been started.");
         int capacity = centre.getCapacity();
-        AtomicInteger currentLoading = centre.getCurrentLoading();
-        while (currentLoading.get() < capacity * MIN_PERMISSIBLE_LOADING_PERCENT / 100) {
+        int currentLoading = centre.getCurrentLoading().get();
+        while (currentLoading < capacity * MIN_PERMISSIBLE_LOADING_PERCENT / 100) {
             int storedCargo = capacity * CONTROLLER_ACTION_PERCENT / 100;
             centre.storeCargoInCentre(storedCargo);
             controllerBalance = controllerBalance - storedCargo;
-            currentLoading = centre.getCurrentLoading();
+            currentLoading = centre.getCurrentLoading().get();
             logger.log(Level.DEBUG, "The logistic centre has been stored by {}.",
                     storedCargo);
         }
-        while (currentLoading.get() > capacity * MAX_PERMISSIBLE_LOADING_PERCENT / 100) {
+        while (currentLoading > capacity * MAX_PERMISSIBLE_LOADING_PERCENT / 100) {
             int unloadedCargo = capacity * CONTROLLER_ACTION_PERCENT / 100;
             centre.unloadCargoFromCentre(unloadedCargo);
             controllerBalance = controllerBalance + unloadedCargo;
-            currentLoading = centre.getCurrentLoading();
+            currentLoading = centre.getCurrentLoading().get();
             logger.log(Level.DEBUG, "The logistic centre has been unloaded by {}.",
                     unloadedCargo);
         }
